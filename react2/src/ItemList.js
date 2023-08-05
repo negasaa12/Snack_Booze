61abcf0143e17c086d71ff56af676582a14acf58
kelly <sntsklvn@gmail.com>
2023-08-05 10:55:07 -0400

    made style for itemList component

--- react2/src/ItemList.js
+++ react2/src/ItemList.js
@@ -1,18 +1,36 @@
 import React from "react";
-
+import "./itemList.css";
 
 
 const ItemList = ({items})=>{
 
-        
+    const totalSnacks = items.snacks.length;
+    const totalDrinks = items.drinks.length;
+    const totalItems = totalSnacks + totalDrinks;
 
-    return(
-        <>
-        <h2> Snack & Booze </h2>
-        
-        <h3> We have {items.snacks.length} Snacks & {items.drinks.length} Drinks</h3>
-        </>
-    )
-}
+    return (
+        <div className="itemList-container">
+          <h3 className="itemList-h3">
+            Welcome to Snacks & Booze
+          </h3>
+          <p className="itemList-p">
+            We offer a wide variety of {totalItems} delicious treats, including:
+          </p>
+          <ul className="itemList-ul">
+            <li className="itemList-li">
+              <strong>{totalSnacks} Kinds of Snacks</strong>
+            </li>
+            <li>
+              <strong>{totalDrinks} Refreshing Drinks</strong>
+            </li>
+          </ul>
+          <p className="itemList-p">
+            Whether you're craving something savory or looking for a refreshing beverage,
+            we have something for everyone! Come and explore our delectable selection today.
+          </p>
+        </div>
+      );
+    };
+
 
 export default ItemList;
